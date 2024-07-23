8118b45dadc548c8721f63bbebd911e85933c01c
AlexandreTonin <xandetonin@gmail.com>
2024-07-23 14:18:16 -0300

    upload project

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,22 @@
 import { Helmet, HelmetProvider } from 'react-helmet-async'
 import { RouterProvider } from 'react-router-dom'
+import { Toaster } from 'sonner'
 
+import { ThemeProvider } from './components/theme/theme-provider'
 import { router } from './pages/routes'
+import { QueryClientProvider } from '@tanstack/react-query'
+import { queryClient } from './lib/react-query'
 
 export function App() {
   return (
     <HelmetProvider>
-      <Helmet titleTemplate="%s | pizza shop" />
-      <RouterProvider router={router} />
+      <ThemeProvider defaultTheme="dark" storageKey="pizzashop-theme">
+        <Helmet titleTemplate="%s | pizza shop" />
+        <Toaster richColors />
+        <QueryClientProvider client={queryClient}>
+        <RouterProvider router={router} />
+        </QueryClientProvider>
+      </ThemeProvider>
     </HelmetProvider>
   )
 }
--- src/pages/routes.tsx
+++ src/pages/routes.tsx
@@ -2,18 +2,28 @@ import { createBrowserRouter } from 'react-router-dom'
 
 import { AppLayout } from './_layouts/app'
 import { AuthLayout } from './_layouts/auth'
-import { Dashboard } from './app/dashboard'
+import { NotFound } from './404'
+import { Dashboard } from './app/dashboard/dashboard'
+import { Orders } from './app/orders/orders'
 import { SignIn } from './auth/sign-in'
+import { SignUp } from './auth/sign-up'
 
 export const router = createBrowserRouter([
   {
     path: '/',
     element: <AppLayout />,
-    children: [{ path: '/', element: <Dashboard /> }],
+    errorElement: <NotFound />,
+    children: [
+      { path: '/', element: <Dashboard /> },
+      { path: '/orders', element: <Orders /> },
+    ],
   },
   {
     path: '/',
     element: <AuthLayout />,
-    children: [{ path: '/sign-in', element: <SignIn /> }],
+    children: [
+      { path: '/sign-in', element: <SignIn /> },
+      { path: '/sign-up', element: <SignUp /> },
+    ],
   },
 ])
