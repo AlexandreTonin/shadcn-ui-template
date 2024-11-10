9834d33b1991f7eb6afe8d906c9d1d5480d33aae
Alexandre Tonin <xandetonin@gmail.com>
2024-11-10 14:27:48 -0300

    fix: ring color adjustment

--- src/pages/app/orders/orders.tsx
+++ src/pages/app/orders/orders.tsx
@@ -3,7 +3,7 @@ import { Helmet } from 'react-helmet-async'
 export function Products() {
   return (
     <>
-      <Helmet title="Pedidos" />
+      <Helmet title="Products" />
       <div className="flex flex-col gap-4">
         <h1 className="text-3xl font-bold tracking-tight">Products</h1>
       </div>
